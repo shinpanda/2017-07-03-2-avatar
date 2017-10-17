711dd584e00b7df2f641c089c4157c26eb5bd43d
user <user@User84>
2017-10-17 17:51:04 +0900

    메인페이지 디자인 중

---
+++ src/main/java/com/bepum/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.bepum.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class HomeController {
+	
+	@RequestMapping("index")
+	public String index() {
+		return "index";
+	}
+}
