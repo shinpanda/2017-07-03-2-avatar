856599cb6137c2f576cfa194dc08d1edc9ee6239
user <user@user96>
2017-11-13 18:34:06 +0900

    aa

--- src/main/java/com/avatar/web/dao/MemberClassDao.java
+++ src/main/java/com/avatar/web/dao/MemberClassDao.java
@@ -1,7 +1,15 @@
 package com.avatar.web.dao;
 
+import java.util.List;
+
+import com.avatar.web.entity.MemberClassView;
+
 public interface MemberClassDao {
 	int insert(String memberId, String classId);
 
 	String getClassId(String id);
+	
+	List<MemberClassView> getList(String classId);
+
+	List<MemberClassView> getHeader(String sessionId);
 }
